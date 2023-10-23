51dc5c39b233839ad08cd1ad581c73460f57211c
zoro <adityasonone369@gmail.com>
2023-10-23 19:48:07 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Jotter_Notes_App
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://inviting-flamingo-26.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
